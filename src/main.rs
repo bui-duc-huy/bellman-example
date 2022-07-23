635ec13e890bd55358018f448ce51c49a7bd2b16
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-07-23 09:12:38 +0700

    update code

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,41 @@ use bellman::{
     groth16::{
         generate_random_parameters,
         create_random_proof,
-        prepare_verifying_key
+        prepare_verifying_key,
+        verify_proof
     }
 };
 use rand::rngs::OsRng;
-use bls12_381::Bls12;
+use bls12_381::{Bls12, Scalar};
 use sha2::{Digest, Sha256};
 
 mod problem;
 
 fn main() {
+    println!("Learn zk-SNARKs with Terry");
     let params = {
         let c = problem::OurProblem { value: None };
 
         generate_random_parameters::<Bls12, _, _>(c, &mut OsRng).unwrap()
     };
 
+    println!("Prepare key...");
     let pvk = prepare_verifying_key(&params.vk);
 
+    println!("Prepare input...");
     let hidden_value = [40; 80];
     let hash_bit = bytes_to_bits_le(&Sha256::digest(&hidden_value));
-    let _inputs = compute_multipacking(&hash_bit);
+    let inputs = compute_multipacking::<Scalar>(&hash_bit);
 
     let c = problem::OurProblem {
         value: Some(hidden_value),
     };
 
-    let proof = create_random_proof(c, &params, &mut OsRng);
+    println!("Create proof...");
+    let proof = create_random_proof(c, &params, &mut OsRng).unwrap();
 
+    println!("Verify proof...");
+    let result = verify_proof(&pvk, &proof, &inputs);
+    
+    println!("Result: {}", result.is_ok());
 }
